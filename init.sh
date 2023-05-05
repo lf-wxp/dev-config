cadfb52989ad36f8ba29a97c193a99988b6998fa
xiaoping wu <franciscowxp@gmail.com>
2023-05-05 11:48:20 +0800

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,16 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew tap homebrew/cask-fonts
+
+brew install --cask font-jetbrains-mono-nerd-font
+
+# node
+brew install nvm
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+brew install nvim
+
+brew install neovide
+
+
